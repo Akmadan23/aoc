353fc8d065dc4b76bbddaeabdf95db70b6eb8ec5
Akmadan23 <azadahmadi@mailo.com>
2023-02-11 18:07:05 +0100

    Add support for 2022 edition

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,20 @@ use bin::*;
 use aoc::{ flush, read };
 
 fn main() {
-    println!("Advent of Code: 2020-2021");
-    println!("-------------------------"); 
+    println!("Advent of Code: 2020..=2022");
+    println!("---------------------------");
 
     loop {
         let mut input = String::new();
-        print!("\nSelect a year between 2020 and 2021: ");
+        print!("\nSelect a year between 2020 and 2022 (0 to exit): ");
         flush!();
         read!(input);
 
         if let Ok(year) = input.trim().parse() {
             match year {
-                2020..=2021 => loop {
+                2020..=2022 => loop {
                     let mut input = String::new();
-                    print!("\nSelect a day between 1 and 25: ");
+                    print!("\n[{}] Select a day between 1 and 25: ", year);
                     flush!();
                     read!(input);
 
@@ -27,6 +27,7 @@ fn main() {
                                 2 => day2020_02::main(),
                                 3 => day2020_03::main(),
                                 4 => day2020_04::main(),
+                                5..=25 => println!("Work in progress..."),
                                 0 => break,
                                 _ => continue
                             },
@@ -39,9 +40,16 @@ fn main() {
                                 5 => day2021_05::main(),
                                 6 => day2021_06::main(),
                                 7 => day2021_07::main(),
+                                8..=25 => println!("Work in progress..."),
                                 0 => break,
                                 _ => continue
                             },
+
+                            2022 => match day {
+                                1..=25 => println!("Work in progress..."),
+                                0 => break,
+                                _ => continue
+                            }
 
                             _ => break
                         }
