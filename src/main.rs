f305fbd03a503b0c2ce656a55283ae182babaa76
Akmadan23 <azadahmadi@mailo.com>
2023-08-05 21:24:07 +0200

    Rewrite using less indentations

--- src/main.rs
+++ src/main.rs
@@ -12,59 +12,63 @@ fn main() {
         flush!();
         read!(input);
 
-        if let Ok(year) = input.trim().parse() {
-            match year {
-                2020..=2022 => loop {
-                    let mut input = String::new();
-                    print!("\n[{}] Select a day between 1 and 25: ", year);
-                    flush!();
-                    read!(input);
+        let Ok(year) = input.trim().parse() else {
+            continue;
+        };
 
-                    if let Ok(day) = input.trim().parse() {
-                        match year {
-                            2020 => match day {
-                                1 => day2020_01::main(),
-                                2 => day2020_02::main(),
-                                3 => day2020_03::main(),
-                                4 => day2020_04::main(),
-                                5..=25 => println!("Work in progress..."),
-                                0 => break,
-                                _ => continue
-                            },
+        match year {
+            2020..=2022 => loop {
+                let mut input = String::new();
+                print!("\n[{}] Select a day between 1 and 25 (0 to change year): ", year);
+                flush!();
+                read!(input);
 
-                            2021 => match day {
-                                1 => day2021_01::main(),
-                                2 => day2021_02::main(),
-                                3 => day2021_03::main(),
-                                4 => day2021_04::main(),
-                                5 => day2021_05::main(),
-                                6 => day2021_06::main(),
-                                7 => day2021_07::main(),
-                                8..=25 => println!("Work in progress..."),
-                                0 => break,
-                                _ => continue
-                            },
+                let Ok(day) = input.trim().parse() else {
+                    continue;
+                };
 
-                            2022 => match day {
-                                1 => day2022_01::main(),
-                                2 => day2022_02::main(),
-                                3 => day2022_03::main(),
-                                4 => day2022_04::main(),
-                                5 => day2022_05::main(),
-                                6 => day2022_06::main(),
-                                7..=25 => println!("Work in progress..."),
-                                0 => break,
-                                _ => continue
-                            }
+                match year {
+                    2020 => match day {
+                        1 => day2020_01::main(),
+                        2 => day2020_02::main(),
+                        3 => day2020_03::main(),
+                        4 => day2020_04::main(),
+                        5..=25 => println!("Work in progress..."),
+                        0 => break,
+                        _ => continue
+                    },
 
-                            _ => break
-                        }
-                    }
-                },
+                    2021 => match day {
+                        1 => day2021_01::main(),
+                        2 => day2021_02::main(),
+                        3 => day2021_03::main(),
+                        4 => day2021_04::main(),
+                        5 => day2021_05::main(),
+                        6 => day2021_06::main(),
+                        7 => day2021_07::main(),
+                        8..=25 => println!("Work in progress..."),
+                        0 => break,
+                        _ => continue
+                    },
 
-                0 => break,
-                _ => continue
-            }
+                    2022 => match day {
+                        1 => day2022_01::main(),
+                        2 => day2022_02::main(),
+                        3 => day2022_03::main(),
+                        4 => day2022_04::main(),
+                        5 => day2022_05::main(),
+                        6 => day2022_06::main(),
+                        7..=25 => println!("Work in progress..."),
+                        0 => break,
+                        _ => continue
+                    },
+
+                    _ => break
+                }
+            },
+
+            0 => break,
+            _ => continue
         }
     }
 }
